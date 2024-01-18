3954e59f06d52b0ab8c1f4007bb8d67a33e50f74
Adrian Ramirez <ctw03239@criticaltechworks.com>
2024-01-18 14:53:01 +0000

    First commit

---
+++ src/test/java/com/pt/ara/quarkus/learning/GreetingResourceTest.java
@@ -0,0 +1,20 @@
+package com.pt.ara.quarkus.learning;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class GreetingResourceTest {
+    @Test
+    void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy and word"));
+    }
+
+}
