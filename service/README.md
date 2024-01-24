1093306956f792737dfe0eb3c58be5fed6ff1f5b
Adrian Ramirez <ctw03239@criticaltechworks.com>
2024-01-24 17:09:02 +0000

    Configuring opentracing (quarkus.jaeger)

--- service/README.md
+++ service/README.md
@@ -1,5 +1,7 @@
 # service
 
+> **_NOTE:_**  JAEGER UI at http://localhost:16686/search.
+
 This project uses Quarkus, the Supersonic Subatomic Java Framework.
 
 If you want to learn more about Quarkus, please visit its website: https://quarkus.io/ .
--- service/src/main/java/pt/ara/learnig/RoomResource.java
+++ service/src/main/java/pt/ara/learnig/RoomResource.java
@@ -4,6 +4,7 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.jboss.logging.Logger;
 
 import java.util.List;
 
@@ -11,13 +12,16 @@ import java.util.List;
 public class RoomResource {
     @Inject
     RoomService roomService;
+    private final static Logger LOG = Logger.getLogger(RoomResource.class);
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Room> getAllRooms(@QueryParam("bedInfo") String bedInfo) {
-        if (!bedInfo.isBlank()) {
+        LOG.info("entering getAllRooms");
+        if (bedInfo != null && !bedInfo.isBlank()) {
             return roomService.getRoomsByBedInfo(bedInfo);
         }
+        LOG.info("exiting getAllRooms");
         return roomService.getAllRooms();
     }
 
