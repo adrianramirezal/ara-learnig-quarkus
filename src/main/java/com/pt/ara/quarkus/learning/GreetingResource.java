82a4890e1e38221cbe11dbda5c04b2f33239bd54
Adrian Ramirez <ctw03239@criticaltechworks.com>
2024-01-24 10:03:39 +0000

    External Datasource

--- src/main/java/com/pt/ara/quarkus/learning/GreetingResource.java
+++ src/main/java/com/pt/ara/quarkus/learning/GreetingResource.java
@@ -8,13 +8,24 @@ import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
-@Path("/hello")
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/lq")
 public class GreetingResource {
 
     private final GreetingConfig config;
     private final String message;
 
     private static final Logger LOGGER = Logger.getLogger(GreetingResource.class);
+
+    @Inject
+    DataSource dataSource;
 
     @Inject
     public GreetingResource(
@@ -27,10 +38,35 @@ public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @Path("/hello")
     public String hello() {
         LOGGER.debug("@@@ Start");
         String msg = "Hello " + config.getRecipient() + " " + message;
         LOGGER.info("@@@ message: " + msg);
         return msg;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/rooms")
+    public List<Room> getRooms() {
+        LOGGER.debug("@@@ Start Select");
+        String sql = "SELECT NAME, ROOM_NUMBER, BED_INFO FROM ROOM";
+        List<Room> rooms = new ArrayList<>();
+        try {
+            Connection connection = dataSource.getConnection();
+            try (Statement stmt = connection.createStatement()) {
+                ResultSet rs = stmt.executeQuery(sql);
+                while (rs.next()) {
+                    rooms.add(new Room(rs.getString("NAME"), rs.getString("ROOM_NUMBER"),
+                            rs.getString("BED_INFO")));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        rooms.forEach(System.out::println);
+        LOGGER.debug("@@@ End Select");
+        return rooms;
     }
 }
---
+++ src/main/java/com/pt/ara/quarkus/learning/Room.java
@@ -0,0 +1,23 @@
+package com.pt.ara.quarkus.learning;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Room {
+    private String name;
+    private String roomNumber;
+    private String bedInfo;
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "name='" + name + '\'' +
+                ", roomNumber='" + roomNumber + '\'' +
+                ", bedInfo='" + bedInfo + '\'' +
+                '}';
+    }
+}
