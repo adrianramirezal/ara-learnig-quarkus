eddc15882ce0f25449193f1a9376364b36d8ea5c
Adrian Ramirez <ctw03239@criticaltechworks.com>
2024-01-24 17:25:01 +0000

    Configuring a MeterRegistry (quarkus-micrometer, micrometer-registry-prometheus)

--- service/src/main/java/pt/ara/learnig/RoomResource.java
+++ service/src/main/java/pt/ara/learnig/RoomResource.java
@@ -1,5 +1,7 @@
 package pt.ara.learnig;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
@@ -7,21 +9,28 @@ import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.jboss.logging.Logger;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Path("/rooms")
 public class RoomResource {
     @Inject
     RoomService roomService;
+    @Inject
+    MeterRegistry meterRegistry;
+
     private final static Logger LOG = Logger.getLogger(RoomResource.class);
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Room> getAllRooms(@QueryParam("bedInfo") String bedInfo) {
+        Timer timer = Timer.builder("roomservice").tag("method", "getAllRooms").register(meterRegistry);
+        long start = System.nanoTime();
         LOG.info("entering getAllRooms");
         if (bedInfo != null && !bedInfo.isBlank()) {
             return roomService.getRoomsByBedInfo(bedInfo);
         }
         LOG.info("exiting getAllRooms");
+        timer.record(System.nanoTime() - start, TimeUnit.NANOSECONDS);
         return roomService.getAllRooms();
     }
 
